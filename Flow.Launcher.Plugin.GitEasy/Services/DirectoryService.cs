92dffc56d71b99b4a41730b40f059cdc759c4710
Andrew SZE-TO <zkwokleung@gmail.com>
2024-07-15 09:25:22 +0800

    feat: added open repo command

---
+++ Flow.Launcher.Plugin.GitEasy/Services/DirectoryService.cs
@@ -0,0 +1,16 @@
+﻿using Flow.Launcher.Plugin.GitEasy.Services.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Flow.Launcher.Plugin.GitEasy.Services;
+
+public class DirectoryService : IDirectoryService
+{
+    public DirectoryService() { }
+
+    public List<string> GetDirectories(string path)
+    {
+        return Directory.GetDirectories(path).ToList() ;
+    }
+}
--- Flow.Launcher.Plugin.GitEasy/Services/Interfaces/ISystemCommandService.cs
+++ Flow.Launcher.Plugin.GitEasy/Services/Interfaces/ISystemCommandService.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 
-namespace Flow.Launcher.Plugin.GitEasy.Services.Interfaces
+namespace Flow.Launcher.Plugin.GitEasy.Services.Interfaces;
+
+public interface ISystemCommandService
 {
-    public interface ISystemCommandService
-    {
-        public void OpenExplorer(string path = "", Action OnCompleted = null);
-        public void OpenVsCode(string path = "", Action OnCompleted = null);
-    }
+    public void OpenExplorer(string path = "", Action OnCompleted = null);
+    public void OpenVsCode(string path = "", Action OnCompleted = null);
 }
