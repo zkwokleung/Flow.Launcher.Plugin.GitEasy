2e34a80a4ae63275166844e888b524a58031f139
Jeremy <jeremy24wu@gmail.com>
2026-05-08 22:20:40 +1000

    support opening in WSL if repo resides in the distro

--- Flow.Launcher.Plugin.GitEasy/Services/SystemCommandService.cs
+++ Flow.Launcher.Plugin.GitEasy/Services/SystemCommandService.cs
@@ -2,6 +2,7 @@ using Flow.Launcher.Plugin.GitEasy.Services.Interfaces;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace Flow.Launcher.Plugin.GitEasy.Services;
 
@@ -22,31 +23,44 @@ public class SystemCommandService : ISystemCommandService
 
     public void OpenVsCode(string path = "", Action OnCompleted = null)
     {
-        ProcessStartInfo info = new()
-        {
-            FileName = "cmd.exe",
-            Arguments = $"/c code \"{path}\"",
-            WindowStyle = ProcessWindowStyle.Hidden,
-        };
-
-        Process.Start(info).WaitForExit();
-
-        OnCompleted?.Invoke();
+        OpenEditor(path, "code", OnCompleted);
     }
 
     public void OpenCursor(string path = "", Action OnCompleted = null)
     {
-        ProcessStartInfo info = new()
+        OpenEditor(path, "cursor", OnCompleted);
+    }
+
+    private static void OpenEditor(string path, string editorCommand, Action OnCompleted)
+    {
+        ProcessStartInfo info;
+
+        if (path.StartsWith(@"\\wsl.localhost\", StringComparison.OrdinalIgnoreCase))
         {
-            FileName = "cmd.exe",
-            Arguments = $"/c cursor \"{path}\"",
-            WindowStyle = ProcessWindowStyle.Hidden,
-        };
+            var withoutPrefix = path.Substring(@"\\wsl.localhost\".Length);
+            var parts = withoutPrefix.Split(new[] { '\\', '/' }, StringSplitOptions.RemoveEmptyEntries);
+            var distro = parts[0];
+            var linuxPath = "/" + string.Join('/', parts.Skip(1));
+
+            info = new ProcessStartInfo
+            {
+                FileName = "wsl.exe",
+                Arguments = $"-d {distro} -- {editorCommand} \"{linuxPath}\"",
+                WindowStyle = ProcessWindowStyle.Hidden,
+            };
+        }
+        else
+        {
+            info = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/c {editorCommand} \"{path}\"",
+                WindowStyle = ProcessWindowStyle.Hidden,
+            };
+        }
 
         Process.Start(info).WaitForExit();
 
         OnCompleted?.Invoke();
     }
-
-
 }
